e613dd20c772214e6871bc33dccff0dfc9dd0054
Verena Isele <v.j.isele@gmail.com>
2021-02-18 12:28:08 +0100

    uuid hinzugefügt und Checbox und Delete über uuid

--- src/App.js
+++ src/App.js
@@ -2,15 +2,26 @@ import ShoppingItem from './ShoppingItem';
 import Headline from './Headline';
 import Form from './Form';
 import { useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
 
 function App() {
   const [shoppingItems, setShoppingItems] = useState([]);
 
   function addShoppingItem(title) {
-    const newShoppingItem = { title: title, isDone: false };
+    const newShoppingItem = { title: title, isDone: false, id: uuidv4() };
     setShoppingItems([...shoppingItems, newShoppingItem]);
   }
 
+  function toggleCheckbox(idToToggle) {
+    const itemToToggle = shoppingItems.map((item) => {
+      if (item.id == idToToggle) {
+        item.isDone = !item.isDone;
+      }
+      return item;
+    });
+    setShoppingItems(itemToToggle);
+  }
+  /*Alte Lösung über Index 
   function toggleCheckbox(indexToToggle) {
     const itemToToggle = shoppingItems.find(
       (item, index) => index === indexToToggle
@@ -21,26 +32,33 @@ function App() {
     const secondHalf = shoppingItems.slice(indexToToggle + 1);
 
     setShoppingItems([...firstHalf, itemToToggle, ...secondHalf]);
-  }
+  } */
 
-  function deleteShoppingItem(indexToDelete) {
+  function deleteShoppingItem(idToDelete) {
     const remainingItems = shoppingItems.filter(
-      (item, index) => index !== indexToDelete
+      (item) => item.id !== idToDelete
     );
     setShoppingItems(remainingItems);
   }
+  /*   Funktion für Delete alle Werte mit Hilfe von Button DeleteAll
+  function deleteAll() {
+    setShoppingItems([]);
+  } */
 
   return (
     <div className="App">
       <Headline name="Harry" />
       <Form onCreateShoppingItem={addShoppingItem} />
 
-      {shoppingItems.map((shoppingItem, index) => (
+      {shoppingItems.map((
+        shoppingItem //würde hier (shoppingItem, id) stehen, wäre der 2.Wert automatisch der index, auch wenn er 'id' heißt.
+      ) => (
         <ShoppingItem
+          key={shoppingItem.id}
           title={shoppingItem.title}
           isDone={shoppingItem.isDone}
-          onToggleItem={() => toggleCheckbox(index)}
-          onDeleteItem={() => deleteShoppingItem(index)}
+          onToggleItem={() => toggleCheckbox(shoppingItem.id)}
+          onDeleteItem={() => deleteShoppingItem(shoppingItem.id)}
         />
       ))}
     </div>
